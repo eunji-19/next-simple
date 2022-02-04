fb7ccca095ae28aa7ce2a67ad859ee0bdc238d51
eyet010 <eyet010@clmns.co.kr>
2022-02-04 13:06:42 +0900

    Global Styles

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,6 @@ import NavBar from "../components/NavBar";
 export default function About() {
   return (
     <div>
-      <NavBar />
       <h3>About</h3>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import NavBar from "../components/NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h3>Hello</h3>
     </div>
   );
