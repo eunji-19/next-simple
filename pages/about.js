09c7e5117a77f9ebcf836b5ff6d3049fb61b7cb9
eyet010 <eyet010@clmns.co.kr>
2022-02-04 14:48:56 +0900

    redirect/rewrite/movie api fetch

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,10 @@
 import NavBar from "../components/NavBar";
+import CustomHeader from "../components/CustomHeader";
 
 export default function About() {
   return (
     <div>
+      <CustomHeader title="About" />
       <h3>About</h3>
     </div>
   );
